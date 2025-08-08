0788bb0ed1daf31fa2a096fdc0feb14c2d3bdffd
ingrun <ingrun@163.com>
2025-08-08 09:13:30 +0800

    添加随机串默认值

--- src/tools/RandomString.vue
+++ src/tools/RandomString.vue
@@ -6,17 +6,18 @@
     <el-form-item label="目标长度">
       <el-input-number v-model="len" :min="1" :max="100" />
     </el-form-item>
-    <el-form-item label="目标字符">
-      <el-input v-model="caseString" />
-    </el-form-item>
+
     <el-form-item>
       <el-button type="primary" @click="generateCase">生成随机字符串</el-button>
     </el-form-item>
   </el-form>
+      <el-form-item label="源字符">
+      <el-input v-model="caseString" />
+    </el-form-item>
   <el-table :data="list" border style="margin-top: 12px">
     <el-table-column prop="char" label="字符串" />
     <el-table-column label="操作" width="80">
-      <template #default="{ row: { char = ''} }">
+      <template #default="{ row: { char = '' } }">
         <el-button size="small" @click="copy(char)">复制</el-button>
       </template>
     </el-table-column>
@@ -25,9 +26,11 @@
 
 <script setup>
 import { ref } from "vue";
-const count = ref(1);
-const len = ref(1);
-const caseString = ref("");
+const count = ref(10);
+const len = ref(20);
+const caseString = ref(
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,:;-_+*/?=()[]{}!@#$%^&|~`\"'\\"
+);
 let list = ref([]);
 
 const generateCase = () => {
@@ -47,7 +50,9 @@ const generateRandomString = (char) => {
 
 const copy = (text) => {
   navigator.clipboard.writeText(text);
-}
+};
+
+generateCase();
 </script>
 
 <style scoped></style>
